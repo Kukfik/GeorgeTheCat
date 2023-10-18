ccf623d1f4696110154abac8bd7c310b6b11b476
Kukfik <87478668485a@gmail.com>
2023-10-18 10:41:20 +0900

    front

--- frontend/app.py
+++ frontend/app.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template
 
 # Create a Flask application
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 # Define a route and a view function
 @app.route('/')
 def hello_world():
-    return render_template("test.html")
+    return render_template("index.html")
+
+# Define a route and a view function
+@app.route('/forms')
+def forms_page():
+    return render_template("form.html")
+
 
 if __name__ == '__main__':
+    
     app.run(debug=True)
+    
+
