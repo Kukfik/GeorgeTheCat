f1ba07b2f60a60d14ad2bdf3a811c0e5b78375c2
Kukfik <87478668485a@gmail.com>
2023-10-19 09:28:40 +0900

    12321

--- frontend/app.py
+++ frontend/app.py
@@ -1,18 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
+def get_response():
+    return "got response"
 # Create a Flask application
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
 # Define a route and a view function
-@app.route('/')
-def hello_world():
+@app.get('/')
+def index_get():
     return render_template("index.html")
 
 # Define a route and a view function
-@app.route('/forms')
+@app.route('/forms', methods=['GET','POST'])
 def forms_page():
+    if request.method == "POST":
+        message = request.form["description"]
     return render_template("form.html")
 
+# @app.post("/predict")
+# def predict():
+#     text = request.get_json().get("message")
+#     # TODO: check if text is valid
+#     response: get_response(text)
+#     message = {"answer": response}
+#     return jsonify(message)
 
 if __name__ == '__main__':
     
